7f4d760dbba4808ceaae9abff9e683e9115ba677
safin777 <sayedislamsafin7770555@gmail.com>
2023-05-12 21:24:49 +0600

    TS-TYPE ALIASES

--- TSP/src/main.ts
+++ TSP/src/main.ts
@@ -22,8 +22,8 @@ setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 let x : number = 5;
 console.log(x);
 
-//number methods in ts 
-
+//number methods in ts ==============================
+ 
 let b : number = 1000.4353773333;
 console.log(b); //1000.4353773333
 console.log(b.toFixed(2)); //1000.44
@@ -42,7 +42,7 @@ let str3 : string = `hello ${str} ${str2}`;
 
 console.log(str3);
 
-//string methods in ts
+//string methods in ts ==============================
 console.log(str.charAt(0)); //h
 console.log(str.charCodeAt(0)); //104 (ASCII value of h)
 console.log(str.concat(" ", "world")); //hello world
@@ -60,5 +60,179 @@ console.log(str.toLocaleUpperCase()); //HELLO WORLD
 console.log(str.concat(" ", str2)); //hello world Sayed
 console.log(str.trim()); //hello world
 console.log(str.endsWith('d')); //hello world end with d ,so its true 
+
+
+//special type in ts ============================
+let special_data : any = 5;
+//special_data = "hello world";
+//special_data = true;
+console.log(special_data);
+
+//type never
+
+let tnever: never = true; 
+tnever  =  'example of type never'
+console.log(tnever);
+
+//type null
+//type undefined
+
+
+//array in ts ============================
+
+let names : string[] = ["Sayed", "Ali", "Hassan"];
+let names2 : string[] = ["Faiza", "Sara", "Sana"];
+
+let numbers : number[] = [1, 2, 3, 4, 5];
+
+let n : string[]
+
+//array methods in ts ============================
+names.push("ismail"); //add ismail to the end of the array
+names.pop(); //remove the last element from the array
+names.shift(); //remove the first element from the array
+names.unshift("ismail"); //add ismail to the beginning of the array
+n = names.concat(names2); //concatenate two arrays
+
+//filter method
+
+let filterName : string[] =  n.filter((name)=>{
+     if (name.endsWith('a')) {
+         return name;
+     }
+})
+
+console.log("filterd array:",filterName);
+
+//map method
+
+let mapName : string[] =  n.map((name)=>{
+    return name.toUpperCase();
+})
+
+console.log("maped array:",mapName);
+
+
+console.log("[Tuple] in ts ============================");
+
+let infotuple : [string, number, string] = ["Sayed", 25, "Software Engineer"];
+infotuple.push("ismail");
+infotuple.push("Doctor");
+infotuple.push("35");
+
+console.log(infotuple);
+
+//readonly tuple in ts ============================
+
+let readonlytuple : readonly[string, number, string] = ["readonly", 25, "Software Engineer"];
+//readonlytuple.push("ismail"); //error
+//readonlytuple.push("Doctor"); //error
+//readonlytuple.push("35"); //error
+
+console.log(readonlytuple);
+
+
+//object in ts ============================
+
+const tsobject :  { type : string, name: string, age: number, isMarried: boolean } = {
+    type : "active",
+    name : "Sayed",
+    age : 25,
+    isMarried : false
+    
+}
+
+console.log(tsobject);
+
+
+//enum in ts ============================
+console.log("[enum] in ts ============================");
+
+enum StatusCodes {
+  NotFound = 404,
+  Success = 200,
+  Accepted = 202,
+  BadRequest = 400
+}
+
+console.log(StatusCodes);
+
+//use enum in switch case
+
+enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 4
+}
+
+let myColor: Color = Color.Green;
+
+switch (myColor) {
+  case Color.Red:
+    console.log("The color is red");
+    break;
+  case Color.Green:
+    console.log("The color is green");
+    break;
+  case Color.Blue:
+    console.log("The color is blue");
+    break;
+  default:
+    console.log("Unknown color");
+}
+
+//use enum in for loop
+
+enum Color2 {
+  Red = 1,
+  Green = 2,
+  Blue = 4
+}
+
+for (let colorName in Color2) {
+  console.log(colorName);
+}
+
+
+//enum in string 
+enum CardinalDirections {
+  North = 'North',
+  East = "East",
+  South = "South",
+  West = "West"
+};
+
+console.log(CardinalDirections);
+
+//TypeScript Type Aliases and Interfaces ============================
+
+console.log("[TypeScript Type Aliases and Interfaces] in ts ============================");
+
+
+//type aliases
+type longString  =  string;
+type longNumber  =  number;
+type longBoolean  =  boolean;
+
+type Car = {
+   name : longString,
+   model : longString,
+   year : longNumber,
+   isUsed : longBoolean
+  }
+
+const car : Car = {
+  name : "BMW",
+  model : "X5",
+  year : 2020,
+  isUsed : false
+}
+
+console.log(car);
+
+
+
+
+//interfaces
 
 
